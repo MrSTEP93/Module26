03b1f55cab0dd0fd34611e521f47d705590989d2
Shatalov.AG <mrstep93@ya.ru>
2024-07-17 00:03:55 +0300

    28.4

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,15 @@
+﻿let age = prompt("Пожалуйста, введите ваш возраст:");
+if (age >= 18) {
+    alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
+}
+else {
+    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены на google.com");
+    window.location.href = "http://www.google.com"
+}
+/*
+let firstName = 'Ivan';
+
+let result = `${firstName} glad to see you!`;
+console.log(result);
+
+*/
