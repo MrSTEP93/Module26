9d39e728e06a189bf51bc9affbcca3f71889d84f
Shatalov.AG <mrstep93@ya.ru>
2024-07-20 15:50:36 +0300

    29 щт еру цфн

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,35 +1,29 @@
-﻿function SaveSession() {
+﻿function handleSession() {
     let sess = new Map();
-    sess.set('date', Date().toLocaleString());
     sess.set('userAgent', window.navigator.userAgent);
     let userName = prompt('What is your name?');
     sess.set('userName', userName)
-    let age = CheckAge();
-    sess.set('age', age);
-    for (let el of sess) {
-        console.log(el);
-    }
-}
-
-function CheckAge() {
     let age = prompt("Пожалуйста, введите ваш возраст:");
+    sess.set('age', age);
+    let creationDate = Date().toLocaleString();
+    sess.set('creationDate', creationDate);
     if (age >= 18) {
-        alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
+        alert("Приветствуем на LifeSpot! " + creationDate);
     }
     else {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены на google.com");
         window.location.href = "http://www.google.com"
     }
-    return age;
+    return sess;
 }
 
 function FilterContent()
 {
     console.log('Func start!');
-    let userInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+    //let userInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+    let userInput = inputParsefunction();
     let videoCont = document.getElementsByClassName('video-container');
     for (let i = 0; i < videoCont.length; i++) {
-        //console.log(videoCont[i]);
         let children = videoCont[i];
         let videoDesc = children.getElementsByTagName('h3')[0].innerText.toLowerCase();
         console.log(videoDesc + ' includes ' + userInput + '??');
@@ -41,7 +35,16 @@ function FilterContent()
         }
     }
     console.log('Func end.');
+}
 
+let inputParsefunction = function GetUserInput() {
+    return document.getElementsByTagName('input')[0].value.toLowerCase()
+}
+
+var sessionLog = function (session) {
+    for (let el of session) {
+        console.log(el);
+    }
 }
 /*
 let firstName = 'Ivan';
