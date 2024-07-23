7d5f70e26733b6dd56e88117ce28e5e855870d21
Shatalov.AG <mrstep93@ya.ru>
2024-07-23 23:16:27 +0300

    module 30 ended

--- LifeSpot/Static/JS/feedback.js
+++ LifeSpot/Static/JS/feedback.js
@@ -26,11 +26,27 @@ function getReview() {
         if (confirm("Do you want people to rate your comment?")) {
             let review = Object.create(comment);
             review.rate = 0;
+            review.id = Math.floor(Math.random() * 1000);
             PrintComment(review);
         } else {
             PrintComment(comment);
         }
     }
+}
+
+function rate(rate, id) {
+    let sign = '';
+    if (rate == 1) {
+        sign = '+';
+    }
+    let searchingId = id;
+    let element = document.getElementById(searchingId);
+    let arr = element.innerText.split(' ');
+    let score = parseInt(arr[arr.length - 1], 10);
+    score += rate;
+    arr[arr.length - 1] = sign + score;
+    element.innerText = arr.join(' ');
+    //document.getElementById(id).innerText = ' 👍 +' + rate;
 }
 
 const PrintComment = (review) => {
@@ -41,9 +57,12 @@ const PrintComment = (review) => {
 
      let newReview = '<p class="review-name"> <b>' + review["userName"] + '</b> </p> ' +
         '<p class="review-date"> ' + review["date"] + ' </p>' +
-        '<div class="review-content"> ' + review["text"] + '</div><br />';
+        '<div class="review-content"> ' + review["text"] + '</div>';
     if (review.hasOwnProperty('rate')) {
-        newReview += '<div class="review-rating"> <a>-</a> <b>' + review["rate"] + '</b> <a>+</a> </div> <br />';
+        //newReview += '<br /><div class="review-rating"> <a>-</a> <b>' + review["rate"] + '</b> <a>+</a> </div> <br />';
+        newReview += `<br />
+        <button id="dislike-${review["id"]}" onClick="rate(-1, this.id)"> 💩 -${review["rate"]} </button>
+        <button id="like-${review["id"]}" onClick="rate(+1, this.id)"> 👍 +${review["rate"]} </button><br />`;
     }
     document.getElementsByClassName('reviews')[0].innerHTML += newReview;
 };
